077c39588a769b8bb86e545e82fb4056dcd928d2
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-02-26 17:03:59 -0300

    modulo markdown

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('fs'); // file system
-const colors = require ('colors'); // modulo para darle color a mi texto. Lo llamo colocando . + el nombre del color (.green)
-const path = require('path'); // modulo para trabajar con rutas de archivos y directorios
-const marked = require('marked'); // modulo para links buenos
-
+// const colors = require ('colors');
+// modulo para darle color a mi texto. Lo llamo colocando . + el nombre del color (.green)
+// const path = require('path'); // modulo para trabajar con rutas de archivos y directorios
+// const marked = require('marked'); // modulo para links buenos
+// const mdLinks = require("md-links-extractor"); //modulo para extraer enlaces de un archivo md
+// const markdownLinkExtractor = require('markdown-link-extractor');
+// const fetch = require('node-fetch');
 
 /*
 module.exports = () => {
@@ -22,19 +25,19 @@ fs.readlink(file, (err, data1) => {
   if (err) throw err;
   console.log(data1);
 });
-*//
- // Valido si es archivo ******
+*/
+// Valido si es archivo ******
 const readFile = (path) => {
   if (fs.existsSync(path)) {
-      console.log('existe la ruta');
-      // Verificamos que sea un archivo, por que una ruta puede ser valida, pero ser una carpeta
-      if(fs.statSync(path).isFile()) {
-          // Verificamos que sea un archivo .md
-          const arrCadenas = path.split('.');
-          if (arrCadenas[1] === 'md'){
-              return true;
-          }
+    console.log('existe la ruta');
+    // Verificamos que sea un archivo, por que una ruta puede ser valida, pero ser una carpeta
+    if (fs.statSync(path).isFile()) {
+      // Verificamos que sea un archivo .md
+      const arrCadenas = path.split('.');
+      if (arrCadenas[1] === 'md') {
+        return true;
       }
+    }
   }
   return false;
 };
